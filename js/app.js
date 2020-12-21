e34e009f73cc31013d2fa41f19e3eaaead7ee282
Segni Adeba <se.segni.adeba@gmail.com>
2020-12-21 16:53:19 +0400

    getting input values done, showing error message done

--- js/app.js
+++ js/app.js
@@ -2,3 +2,20 @@ const eventBrite = new EventBrite();
 const ui = new UI(eventBrite.getCategories());
 
 
+document.querySelector('#submitBtn')
+    .addEventListener('click', e => {
+        e.preventDefault();
+
+        // read input values
+        const eventName = document.querySelector('input#event-name').value.trim();
+        const category = document.querySelector('select#category').value;
+        
+        if(eventName !== ''){
+            // query event brite api
+        }else{
+            // print message(error)
+            ui.printMessage('Add an Event or City', 'alert alert-danger mt-4 text-center');
+        }
+    });
+
+
--- js/ui.js
+++ js/ui.js
@@ -3,6 +3,7 @@ class UI {
         // App instialization
         this.data = data; 
         this.categoriesSelect = document.querySelector('select#category');
+        this.searchDiv = document.querySelector('#search-events');
         this.init();
     }
 
@@ -21,6 +22,21 @@ class UI {
                 this.categoriesSelect.appendChild(option);
             });
         }).catch(err => console.log(err));
+    }
+
+    printMessage(message, className){
+        const div = document.createElement('div');          // create a div
+        div.className = className;                          // add the class names passed down, for bootstrap styling
+        div.appendChild(document.createTextNode(message));  // append the message passed down to the new div
+        this.searchDiv.appendChild(div);
+        setTimeout(this.removeMessage, 3000);
+    }
+
+    removeMessage(){
+        const alert = document.querySelector('.alert');
+        if(alert){
+            alert.remove();
+        }
     }
 
 
